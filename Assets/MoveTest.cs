1f47989f9ffa1705c7068171cd64cae49da027e5
shoKikkawa <shotaKikkawa@yoshikawashouta-no-MacBook-Air.local>
2016-01-11 22:29:20 +0900

    MoveTest更新,ターゲットまで瞬間移動する

--- Assets/MoveTest.cs
+++ Assets/MoveTest.cs
@@ -2,53 +2,60 @@
 using System.Collections;
 
 public class MoveTest : ChrAttribute {
-	float yPos;
-
 	private Transform player;
 
+	public float speed = 10f;
+
 	private Vector3 targetPosition;
+
+	private float changeTargetSqrDistance = 8f;
+
 	// Use this for initialization
 	void Start () {
-		// xtends variable
+		// extends variable
 
 		initPos = new Vector3 (5,1.2f,-2);
 		setMoveFlag = true;
 		setFallFlag = false;
 
 		// member variable
-		this.transform.position = initPos;
-		this.yPos = transform.position.y;
+		transform.position = initPos;
+		//this.yPos = transform.position.y;
 
-		this.targetPosition = GetRandomPositionOnLevel ();
-		this.player = GameObject.FindWithTag ("Player").transform;
+		targetPosition = GetRandomPositionOnLevel ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (this.transform.position.y < fallPoint) {
+		if (transform.position.y < fallPoint) {
 			setFallFlag = true;
-			this.transform.position = initPos;
+			transform.position = initPos;
 		}
 		if (getMoveFlag)
 			Move();
 	}
 
 	void FixedUpdate(){
-		this.yPos = transform.position.y;
+		
 	}
 	private void Move(){
-		GetRandomPositionOnLevel ();
-//		var hor = Input.GetAxis ("Horizontal");
-//		var ver = Input.GetAxis ("Vertical");
-//		var xpos = transform.position.x + hor * Constants.MOVESPEED;
-//		var zpos = transform.position.z + ver * Constants.MOVESPEED;
-//		transform.position = new Vector3(xpos,transform.position.y,zpos);
+		var horizontalPosition = transform.position.x + Constants.MOVESPEED;
+		var verticalPosition = transform.position.z + Constants.MOVESPEED;
+
+		float sqrDistanceToTarget = Vector3.Magnitude (transform.position - targetPosition);
+		if (sqrDistanceToTarget < changeTargetSqrDistance) {
+			targetPosition = GetRandomPositionOnLevel ();
+		}
+		transform.position = targetPosition;
+		print (sqrDistanceToTarget);
+//		transform.Translate(targetPosition,Space.World);
+		transform.rotation = Quaternion.identity;
 	}
 	public Vector3 GetRandomPositionOnLevel(){
 		float xMin = 1.2f;
-		float xMax = 8.0f;
-		float zMin = -5.5f;
-		float zMax = 3.4f;
+		float xMax = 8f;
+		float zMin = -5f;
+		float zMax = 3f;
 		return new Vector3 (Random.Range (xMin, xMax), 1, Random.Range (zMin, zMax));
 	}
 }
